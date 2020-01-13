8a83e0769689cd5d57d38c9dcd058b90f806232e
YaaNuamah <yaa_fordjour@hotmail.com>
2020-01-13 17:10:30 +0000

    removed code routes in app-routing module|

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NewPollComponent } from './new-poll/new-poll.component';
-import { ResponsesComponent } from './responses/responses.component';
-import { SubmissionComponent } from './submission/submission.component';
-import { UseranswerComponent } from './useranswer/useranswer.component';
 
 
 
 const routes: Routes = [
-  {path: '', redirectTo: '/useranswer', pathMatch: 'full'},
-  
-
 ];
 
 @NgModule({
