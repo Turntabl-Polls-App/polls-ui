3a7a58ca2ffac909c868f6068c0035426e7d438e
YaaNuamah <yaa_fordjour@hotmail.com>
2020-01-13 11:35:55 +0000

    edited the poll data populated in chart

--- src/app/responses/responses.component.ts
+++ src/app/responses/responses.component.ts
@@ -12,38 +12,26 @@ export class ResponsesComponent {
   constructor() {
     //STEP 2 - Chart Data
     const chartData = [{
-      label: "Venezuela",
-      value: "290"
+      label: "YES",
+      value: "60%"
     }, {
-      label: "Saudi",
-      value: "260"
+      label: "NO",
+      value: "20%"
     }, {
-      label: "Canada",
-      value: "180"
+      label: "SOMETIMES",
+      value: "10%"
     }, {
-      label: "Iran",
-      value: "140"
-    }, {
-      label: "Russia",
-      value: "115"
-    }, {
-      label: "UAE",
-      value: "100"
-    }, {
-      label: "US",
-      value: "30"
-    }, {
-      label: "China",
-      value: "30"
+      label: "RARELY",
+      value: "10%"
     }];
     // STEP 3 - Chart Configuration
     const dataSource = {
       chart: {
-        caption: "Countries With Most Oil Reserves [2017-18]",
-        subCaption: "In MMbbl = One Million barrels",
-        xAxisName: "Country",
-        yAxisName: "Reserves (MMbbl)",
-        numberSuffix: "K",
+        caption: "Whether Turntablers like to rest during the Weekend",
+        subCaption: "Options= In Percentages",
+        xAxisName: "Choices",
+        yAxisName: "Percentages (%)",
+        numberSuffix: "%",
         theme: "fusion"
       },
       // Chart Data - from step 2
