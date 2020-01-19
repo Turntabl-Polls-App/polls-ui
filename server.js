e229e7f329c82ce4e059dcdf44c463a008b5d027
agyen <izagyen96gmail.comgit config --global user.email izagyen96gmail.com>
2020-01-19 23:49:17 +0000

    heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/dist/poll-ui/index.html'));
 });
 
