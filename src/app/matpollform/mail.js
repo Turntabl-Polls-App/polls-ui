0e97f78b854cb439e5e9e8b6a9724d02a573d5d9
agyen <izagyen96gmail.comgit config --global user.email izagyen96gmail.com>
2020-01-29 18:21:42 +0000

    update

--- src/app/matpollform/mail.js
+++ src/app/matpollform/mail.js
@@ -129,7 +129,7 @@ async function start(toEmail) {
 									<p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">`+ toEmail.question+`</p>
 									<table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;">
 									  <tbody>
-										<tr *ngFor = "let item of poll">
+										<tr>
 										  <td align="center" style="font-family: sans-serif; font-size: 14px; vertical-align: top; padding-bottom: 15px;">
 											<table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;">
 												<tbody>
@@ -137,7 +137,7 @@ async function start(toEmail) {
 													`<tr>
 												  <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;"> <a href = "https://response-taken.herokuapp.com/?option_id=`+options.option_id+`&poll_id=`+options.poll_id+`" target="_blank" style="display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;"> `+options.content+` </a> </td>
 												</tr><br>`
-												))+ `
+												))+`
 											  </tbody>
 											</table>
 										  </td>
