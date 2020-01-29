6e5c4a22db95ddce0a2ab2f24d2f35a2d5d172c0
agyen <izagyen96gmail.comgit config --global user.email izagyen96gmail.com>
2020-01-29 17:37:24 +0000

    for review

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,27 +15,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 import {MatCardModule, MatDialogModule, MatFormFieldModule, MatIconModule, MatButtonModule, MatInputModule, MatMenuModule, MatToolbarModule, MatSnackBarModule, MatDividerModule} from '@angular/material';
-import { MatpollformComponent } from './matpollform/matpollform.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { ModelComponent } from './model/model.component';
-import { ResponseComponent } from './response/response.component';
-import { ThankyoupageComponent } from './thankyoupage/thankyoupage.component';
+
 
 
 
 // Pass the fusioncharts library and chart module
 @NgModule({
   declarations: [
-    AppComponent,
-    MatpollformComponent,
-    FooterComponent,
-    HeaderComponent,
-    ModelComponent,
-    ResponseComponent,
-    ThankyoupageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -53,9 +40,7 @@ import { ThankyoupageComponent } from './thankyoupage/thankyoupage.component';
     MatToolbarModule,
     MatSnackBarModule,
     MatMenuModule,
-    MatDividerModule,
-    BrowserAnimationsModule, 
-    FormsModule
+    MatDividerModule
     
   ],
   providers: [],
--- src/app/response/response.ts
+++
@@ -1,5 +0,0 @@
-export interface Response{
-    response_id: String,
-    poll_id: String,
-    option_id: String
-}
