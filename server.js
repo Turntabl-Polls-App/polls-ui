8ac6e2558457eca06ef59c4a28b86da822755044
agyen <izagyen96gmail.comgit config --global user.email izagyen96gmail.com>
2020-01-31 10:43:47 +0000

    thank you

--- server.js
+++ server.js
@@ -4,29 +4,16 @@ const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-//const mail = require('./src/app/matpollform/mail');
 const mail = require('./mailgun')
 
 const app = express();
-
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/poll-ui'));
 
 app.use(bodyParser.json());
-// app.use(express.json());
 app.use(cors());
 app.options('*', cors());
-
-// app.post('/sendmail', function(req, res) {
-// 	console.log('sending mail...');
-// 	const user = req.body;
-// 	mail(user);
-// });
-
-app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/dist/poll-ui/index.html'));
-});
 
 app.post('/sendmail', function(req, res) {
 	console.log('sending mail...');
@@ -34,9 +21,15 @@ app.post('/sendmail', function(req, res) {
 	mail(user);
 });
 
-app.get('/thankyoupage', function(req, res){
-	res.sendFile(path.join(__dirname + '/thanks.html'));
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/dist/poll-ui/index.html'));
 });
+
+
+
+// app.get('/thankyoupage', function(req, res){
+// 	res.sendFile(path.join(__dirname + '/thanks.html'));
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080, () => {
