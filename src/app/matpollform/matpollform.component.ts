e048719ff240777839c0088e2fbe34ae7c2fb246
YaaNuamah <yaa_fordjour@hotmail.com>
2020-02-05 09:11:04 +0000

    updated travis to original

--- src/app/matpollform/matpollform.component.ts
+++ src/app/matpollform/matpollform.component.ts
@@ -1,47 +1,52 @@
-import { Component, OnInit, Injectable } from '@angular/core';
-import { UUID } from 'angular2-uuid';
-import {Poll, Options } from  '../model/poll';
-import { AppserviceService } from '../appservice.service';
-import { FormControl, FormGroup } from '@angular/forms';
-import {SendmailService} from './sendmail.service';
-import {User} from '../model/poll';
-import { Observable } from 'rxjs'
+import { Component, OnInit } from "@angular/core";
+import { UUID } from "angular2-uuid";
+import { Poll, Options } from "../model/poll";
+import { AppserviceService } from "../appservice.service";
+import { FormControl, FormGroup } from "@angular/forms";
+import { SendmailService } from "./sendmail.service";
+import { User } from "../model/poll";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-matpollform',
-  templateUrl: './matpollform.component.html',
-  styleUrls: ['./matpollform.component.scss']
+  selector: "app-matpollform",
+  templateUrl: "./matpollform.component.html",
+  styleUrls: ["./matpollform.component.scss"]
 })
-
-  export class MatpollformComponent implements OnInit {
-
+export class MatpollformComponent implements OnInit {
   creatorsForm = new FormGroup({
-    email: new FormControl('')
-    });
+    email: new FormControl("")
+  });
 
-    userObservable: Observable<User>
+  userObservable: Observable<User>;
 
-    pollid 
-    poll: Poll
-  
-    constructor(private appservice : AppserviceService, private sendmailService: SendmailService) { 
-    this.pollid = UUID.UUID()
-     
+  pollid;
+  poll: Poll;
+
+  constructor(
+    private appservice: AppserviceService,
+    private sendmailService: SendmailService
+  ) {
+    this.pollid = UUID.UUID();
+
     this.poll = {
-      recipient_email: '',
+      recipient_email: "",
       poll_id: this.pollid,
-      question:'',
+      question: "",
       options: [],
-      creator_email: '',
-      selectMultiple: false,
+      creator_email: "",
+      selectMultiple: false
     };
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   addOption(): void {
-    this.poll.options.push({ option_id: UUID.UUID(), content: name, poll_id: this.pollid})}
+    this.poll.options.push({
+      option_id: UUID.UUID(),
+      content: name,
+      poll_id: this.pollid
+    });
+  }
 
   remove(option): void {
     const index = this.poll.options.findIndex(x => x.option_id === option.id);
@@ -55,12 +60,15 @@ import { Observable } from 'rxjs'
 
   send() {
     this.appservice.addNewPoll(this.poll).subscribe();
-    this.sendmailService.sendmail(this.poll).subscribe()
+    this.sendmailService.sendmail(this.poll).subscribe();
   }
 
   saveActive(): boolean {
-    return this.poll.question.length > 0 
-      && this.poll.options.length > 0 
-      && this.poll.options.find(x => !x.content || x.content.length === 0) === undefined;
+    return (
+      this.poll.question.length > 0 &&
+      this.poll.options.length > 0 &&
+      this.poll.options.find(x => !x.content || x.content.length === 0) ===
+        undefined
+    );
   }
 }
--- src/assets/script/app.js
+++
@@ -1,9 +0,0 @@
-let newOption = document.querySelector("#newOptionbtn")
-
-console.log("js working...");
-
-newOption.addEventListener('click', addNewOption);
-
-const addNewOption = () => {
-    console.log("Adding new option...");
-}
