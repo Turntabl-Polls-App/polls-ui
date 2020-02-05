993ac16366400bcb4bd582ade2207ce2659dae9b
agyen <izagyen96gmail.comgit config --global user.email izagyen96gmail.com>
2020-02-05 09:41:54 +0000

    travis

--- karma.conf.js
+++ karma.conf.js
@@ -6,32 +6,32 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+    //   require('karma-jasmine'),
+    //   require('karma-chrome-launcher'),
+    //   require('karma-jasmine-html-reporter'),
+    //   require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/tpms'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
+    // client: {
+    //   clearContext: false // leave Jasmine Spec Runner output visible in browser
+    // },
+    // coverageIstanbulReporter: {
+    //   dir: require('path').join(__dirname, './coverage/tpms'),
+    //   reports: ['html', 'lcovonly', 'text-summary'],
+    //   fixWebpackSourcePaths: true
+    // },
+    // reporters: ['progress', 'kjhtml'],
+    // customLaunchers: {
+    //   ChromeHeadlessNoSandbox: {
+    //     base: 'ChromeHeadless',
+    //     flags: ['--no-sandbox']
+    //   }
+    // },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    //browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
